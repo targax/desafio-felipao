1e4544589bb922ba87150bafc856097111528f47
targax <brunotarga08@gmail.com>
2024-10-10 15:02:42 -0300

    codigo de desfio do felipao

--- src/index.js
+++ src/index.js
@@ -1,2 +1,40 @@
-let nome = "Bruno Targa";
-console.log(nome);
+
+let nomeHeroi = "Targa"
+let heroiXp = 5000;
+let nivelHeroi = "";
+
+if(heroiXp < 1000){
+
+    nivelHeroi = "Ferro";
+
+}else if((heroiXp > 1000) && ( heroiXp <= 2000)){
+
+    nivelHeroi = "Bronze";
+
+}else if((heroiXp > 2000) && ( heroiXp <= 5000)){
+    
+    nivelHeroi = "Prata";
+
+}else if((heroiXp > 5000) && ( heroiXp <= 7000)){
+    
+    nivelHeroi = "Ouro";
+    
+}else if((heroiXp > 7000) && ( heroiXp <= 8000)){
+    
+    nivelHeroi = "Platina";
+
+}else if((heroiXp > 8000) && ( heroiXp <= 9000)){
+    
+    nivelHeroi = "Ascendente";
+
+}else if((heroiXp > 9000) && ( heroiXp <= 10000)){
+    
+    nivelHeroi = "Imortal";
+
+}else if(heroiXp >= 10001 ){
+    
+    nivelHeroi = "Radiante";
+
+}
+
+console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivelHeroi)
